511f0f4c49ea0b955a0e2a410bcf6cae0a861356
Manpreet Singh <msingh@6river.com>
2020-09-13 10:51:17 -0400

    init

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -1,39 +1,40 @@
 // PROVIDED CODE BELOW (LINES 1 - 80) DO NOT REMOVE
+const SERVER = 'http://localhost:3000'
 
 // The store will hold all information needed globally
-var store = {
+const store = {
 	track_id: undefined,
 	player_id: undefined,
 	race_id: undefined,
 }
 
 // We need our javascript to wait until the DOM is loaded
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 	onPageLoad()
 	setupClickHandlers()
 })
 
 async function onPageLoad() {
 	try {
-		getTracks()
+		await getTracks()
 			.then(tracks => {
 				const html = renderTrackCards(tracks)
 				renderAt('#tracks', html)
 			})
 
-		getRacers()
+		await getRacers()
 			.then((racers) => {
 				const html = renderRacerCars(racers)
 				renderAt('#racers', html)
 			})
-	} catch(error) {
+	} catch (error) {
 		console.log("Problem getting tracks and racers ::", error.message)
 		console.error(error)
 	}
 }
 
 function setupClickHandlers() {
-	document.addEventListener('click', function(event) {
+	document.addEventListener('click', function (event) {
 		const { target } = event
 
 		// Race track form field
@@ -49,7 +50,7 @@ function setupClickHandlers() {
 		// Submit create race form
 		if (target.matches('#submit-create-race')) {
 			event.preventDefault()
-	
+
 			// start race
 			handleCreateRace()
 		}
@@ -65,7 +66,7 @@ function setupClickHandlers() {
 async function delay(ms) {
 	try {
 		return await new Promise(resolve => setTimeout(resolve, ms));
-	} catch(error) {
+	} catch (error) {
 		console.log("an error shouldn't be possible here")
 		console.log(error)
 	}
@@ -76,10 +77,15 @@ async function delay(ms) {
 async function handleCreateRace() {
 	// render starting UI
 	renderAt('#race', renderRaceStartView())
-
-	// TODO - Get player_id and track_id from the store
+	console.log('store >>>>> ', store);
 	
+	// TODO - Get player_id and track_id from the store
+	const player_id = store.player_id;
+	const track_id = store.track_id;
+
 	// const race = TODO - invoke the API call to create the race, then save the result
+	const race = await axios.post(`${SERVER}/race`);
+	console.log(`race >>>>> `, race);
 
 	// TODO - update the store with the race id
 
@@ -93,21 +99,21 @@ async function handleCreateRace() {
 
 function runRace(raceID) {
 	return new Promise(resolve => {
-	// TODO - use Javascript's built in setInterval method to get race info every 500ms
+		// TODO - use Javascript's built in setInterval method to get race info every 500ms
 
-	/* 
-		TODO - if the race info status property is "in-progress", update the leaderboard by calling:
+		/* 
+			TODO - if the race info status property is "in-progress", update the leaderboard by calling:
+	
+			renderAt('#leaderBoard', raceProgress(res.positions))
+		*/
 
-		renderAt('#leaderBoard', raceProgress(res.positions))
-	*/
-
-	/* 
-		TODO - if the race info status property is "finished", run the following:
-
-		clearInterval(raceInterval) // to stop the interval from repeating
-		renderAt('#race', resultsView(res.positions)) // to render the results view
-		reslove(res) // resolve the promise
-	*/
+		/* 
+			TODO - if the race info status property is "finished", run the following:
+	
+			clearInterval(raceInterval) // to stop the interval from repeating
+			renderAt('#race', resultsView(res.positions)) // to render the results view
+			reslove(res) // resolve the promise
+		*/
 	})
 	// remember to add error handling for the Promise
 }
@@ -127,7 +133,7 @@ async function runCountdown() {
 			// TODO - if the countdown is done, clear the interval, resolve the promise, and return
 
 		})
-	} catch(error) {
+	} catch (error) {
 		console.log(error);
 	}
 }
@@ -137,7 +143,7 @@ function handleSelectPodRacer(target) {
 
 	// remove class selected from all racer options
 	const selected = document.querySelector('#racers .selected')
-	if(selected) {
+	if (selected) {
 		selected.classList.remove('selected')
 	}
 
@@ -152,7 +158,7 @@ function handleSelectTrack(target) {
 
 	// remove class selected from all track options
 	const selected = document.querySelector('#tracks .selected')
-	if(selected) {
+	if (selected) {
 		selected.classList.remove('selected')
 	}
 
@@ -160,7 +166,7 @@ function handleSelectTrack(target) {
 	target.classList.add('selected')
 
 	// TODO - save the selected track id to the store
-	
+
 }
 
 function handleAccelerate() {
@@ -182,7 +188,7 @@ function renderRacerCars(racers) {
 
 	return `
 		<ul id="racers">
-			${reuslts}
+			${results}
 		</ul>
 	`
 }
@@ -305,58 +311,111 @@ function renderAt(element, html) {
 
 // API CALLS ------------------------------------------------
 
-const SERVER = 'http://localhost:8000'
+
 
 function defaultFetchOpts() {
 	return {
 		mode: 'cors',
 		headers: {
 			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin' : SERVER,
+			'Access-Control-Allow-Origin': SERVER,
 		},
 	}
 }
 
 // TODO - Make a fetch call (with error handling!) to each of the following API endpoints 
 
-function getTracks() {
-	// GET request to `${SERVER}/api/tracks`
+async function getTracks() {
+	let response;
+	try {
+		response = await axios.get(`${SERVER}/api/tracks`);
+	} catch (error) {
+		response = error.response
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+	`${JSON.stringify(response?.config.params)} returns http status ` +
+	`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
 }
 
-function getRacers() {
-	// GET request to `${SERVER}/api/cars`
+async function getRacers() {
+	let response;
+	try {
+		response = await axios.get(`${SERVER}/api/cars`);
+	} catch (error) {
+		response = error.response
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+
+	return response.data;
 }
 
-function createRace(player_id, track_id) {
+async function createRace(player_id, track_id) {
 	player_id = parseInt(player_id)
 	track_id = parseInt(track_id)
 	const body = { player_id, track_id }
-	
-	return fetch(`${SERVER}/api/races`, {
-		method: 'POST',
-		...defaultFetchOpts(),
-		dataType: 'jsonp',
-		body: JSON.stringify(body)
-	})
-	.then(res => res.json())
-	.catch(err => console.log("Problem with createRace request::", err))
+	let response;
+	try {
+		response = await axios.post(`${SERVER}/api/races`, body)
+	} catch (error) {
+		response = error.response;
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
 }
 
-function getRace(id) {
-	// GET request to `${SERVER}/api/races/${id}`
+async function getRace(id) {
+	let response;
+	try {
+		response = await axios.get(`${SERVER}/api/races/${id}`);
+	} catch (error) {
+		response = error.response;
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
 }
 
-function startRace(id) {
-	return fetch(`${SERVER}/api/races/${id}/start`, {
-		method: 'POST',
-		...defaultFetchOpts(),
-	})
-	.then(res => res.json())
-	.catch(err => console.log("Problem with getRace request::", err))
+async function startRace(id) {
+	let response;
+	try {
+		return await axios.post(`${SERVER}/api/races/${id}/start`);
+	} catch (error) {
+		response = error.response;
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
 }
 
-function accelerate(id) {
-	// POST request to `${SERVER}/api/races/${id}/accelerate`
-	// options parameter provided as defaultFetchOpts
-	// no body or datatype needed for this request
+async function accelerate(id) {
+	let response;
+	try {
+		response = await axios.post(`${SERVER}/api/races/${id}/accelerate`);
+	} catch (error) {
+		response = error.response;
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
+}
+
+async function createRaceTracks() {
+	let response;
+	try {
+		response = await getTracks()
+	} catch (error) {
+		response = error.response;
+		console.log(`${response?.config.method} ${response?.config.url} with params ` +
+		`${JSON.stringify(response?.config.params)} returns http status ` +
+		`${response?.status} with response: `, response?.data);
+	}
+	return response.data;
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
@@ -16,11 +15,42 @@ app.use('/', express.static(path.join(__dirname, '../client')))
 
 // API calls ------------------------------------------------------------------------------------
 app.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/pages/home.html'));
+	res.sendFile(path.join(__dirname, '../client/pages/home.html'));
 })
 
 app.get('/race', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/pages/race.html'));
+	res.sendFile(path.join(__dirname, '../client/pages/race.html'));
 })
 
+app.get('/api/tracks', async (req, res) => {
+	const response = require('../../data.json');
+	res.send(response.tracks)
+})
+
+app.get('/api/cars', async (req, res) => {
+	const response = require('../../data.json');
+	res.send(response.cars)
+})
+
+app.post('/api/races', async (req, res) => {
+	// 	[POST] api/races Create a race
+	// id: number
+	// track: string
+	// player_id: number
+	// cars: Cars[] (array of cars in the race)
+	// results: Cars[] (array of cars in the position they finished, available if the race is finished)
+
+})
+
+app.post('/api/races/{id}', async (req, res) => {
+	// Post races
+})
+
+app.post('/api/races/{id}/start', async (req, res) => {
+	// Post races
+})
+
+app.post('/api/races/{id}/accelerate', async (req, res) => {
+	// Post races
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
